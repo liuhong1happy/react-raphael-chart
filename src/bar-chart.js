857b8e3fabfb6639227a522b3c00badacff03758
liuhong <liuhong@idealsee.cn>
2016-11-23 09:57:01 +0800

    add .toFtextAutotoHide

--- src/bar-chart.js
+++ src/bar-chart.js
@@ -47,16 +47,26 @@ class BarSerise extends React.Component{
 	handleMouseOut(){
 		var data = this.items;
 		this.attr({"fill": data.color});
-		this.set[1].hide();
+		if(data.textAutoHide){
+			this.set[1].hide();
+		}else{
+			this.set[1].attr({"fill": data.color});
+		}
 	}
 	handleMouseOver(){
 		var data = this.items;
 		data.color = this.attr("fill");
-		this.attr({"fill": data.hoverColor});
-		this.set[1].show();
+		this.attr({"fill":data.hoverColor}).toFront();
+		if(data.textAutoHide){
+			this.set[1].show().toFront();
+		}
+		else{
+			this.set[1].attr({"fill":data.hoverColor}).toFront();
+		}
 	}
     render(){
-	    var {serise,height,xAxis,fontSize} = this.props;
+	    var {serise,height,xAxis,fontSize,textAutoHide} = this.props;
+		serise.textAutoHide = textAutoHide;
 		var data = this.getDrawPoints();
 		var defaultY = height - xAxis.height + 15;
 		var handleMouseOut = this.handleMouseOut;
@@ -68,7 +78,7 @@ class BarSerise extends React.Component{
 						<Rect key={"path"+pos} x={ele._x - ele._width/2} y={defaultY} width={ele._width} height={0} data={{...serise}}
 							attr={{"fill": ele.color || serise.color,"stroke": "none"}} mouseout={handleMouseOut} mouseover={handleMouseOver}
 							animate={Raphael.animation({"y":ele._y,"height":ele._height},500,"<>")} />
-						<Text key={"text"-pos} x={ele._x} y={ele._y-fontSize/2} text={String(ele.y)} attr={{"fill": serise.hoverColor,"font-size": fontSize}} hide={true}/>
+						<Text key={"text"-pos} x={ele._x} y={ele._y-fontSize/2} text={String(ele.y)} attr={{"fill": textAutoHide? serise.hoverColor : (ele.color || serise.color),"font-size": fontSize}} hide={textAutoHide}/>
 						</Set>)
 				})
 			}
@@ -98,17 +108,17 @@ class BarChart extends React.Component{
         return points;
     }
     render(){
-        var {width,height,serises,xAxis,yAxis,grid,fontSize,barWidth} = this.props;
+        var {width,height,serises,xAxis,yAxis,grid,fontSize,barWidth,textAutoHide,children} = this.props;
         return (<Paper width={width} height={height}>
             <Axis type="bar" width={width} height={height} xAxis={xAxis} yAxis={yAxis} grid={grid} />
             {
                 serises.map(function(ele,pos){
                     return (<BarSerise key={pos} ref={"serise"+pos} index={pos} count={serises.length+1} width={width} height={height} barWidth={barWidth} fontSize={fontSize}
-							serise={ele} xAxis={xAxis} yAxis={yAxis} />)
+							textAutoHide={textAutoHide} serise={ele} xAxis={xAxis} yAxis={yAxis} />)
                 })
             }
             {
-                this.props.children
+                children
             }
         </Paper>)
     }
@@ -122,7 +132,8 @@ BarChart.propTypes = {
 	yAxis: React.PropTypes.object, 
 	grid:  React.PropTypes.object,
 	barWidth: React.PropTypes.number,
-	fontSize: React.PropTypes.number
+	fontSize: React.PropTypes.number,
+	textAutoHide: React.PropTypes.bool
 };
 BarChart.defaultProps = { 
 	width: 600, 
@@ -149,7 +160,8 @@ BarChart.defaultProps = {
 		showXGrid: true
 	},
 	barWidth: 20,
-	fontSize: 14
+	fontSize: 14,
+	textAutoHide: false
 };
                 
 module.exports = BarChart;
