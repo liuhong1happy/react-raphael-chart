e8c308ee404db61a9cecb73500a2656e2e450a23
liuhong <liuhong@idealsee.cn>
2016-11-23 18:18:13 +0800

    change version to 0.8.0-rc17

--- example/index.js
+++ example/index.js
@@ -71,16 +71,16 @@ class ExtendLineChart extends React.Component{
 				})
 			},4000)
 		},2000)
-		setInterval(function(){
-			_self.setState({
-				serises: [lineSerise,lineSerise1]
-			})
-		},4000)
 //		setInterval(function(){
 //			_self.setState({
-//				serises: [lineSerise2,lineSerise3]
+//				serises: [lineSerise,lineSerise1]
 //			})
 //		},4000)
+		setInterval(function(){
+			_self.setState({
+				serises: [lineSerise2,lineSerise3]
+			})
+		},4000)
 	}
 	render(){
 		return (<VoronoiLineChart width={500} height={360} serises={this.state.serises} />)
